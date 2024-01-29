a5595a214b1fe4a47c11a17c37a7a5b7e747a458
Martin Mihal <martin.mihal@solargis.local>
2024-01-29 23:52:42 +0100

    a

--- r.r
+++ r.r
@@ -361,7 +361,7 @@ print(OutputCollect)
 
 ## Compare all model one-pagers and select one that mostly reflects your business reality
 print(OutputCollect)
-select_model <- "1_122_7" # Pick one of the models from OutputCollect to proceed
+select_model <- "2_65_1" # Pick one of the models from OutputCollect to proceed
 
 #### Version >=3.7.1: JSON export and import (faster and lighter than RDS files)
 #ExportedModel <- robyn_write(InputCollect, OutputCollect, select_model, export = create_files)
