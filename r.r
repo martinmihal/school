3078c53a7c0e198d52589d1f78d2f5e2629693d0
Martin Mihal <martin.mihal@solargis.local>
2024-02-04 17:31:17 +0100

    a

--- r.r
+++ r.r
@@ -362,10 +362,15 @@ print("order")
 print(meta_top_models_ordered)
 
 best_model_by_metric <- meta_top_models_ordered[, 1]
-best_model_id <- best_model_by_metric$solID
+print("best row")
+print(best_model_by_metric)
+print("tool")
+print(best_model_by_metric[1])
+print(best_model_by_metric[2])
+#best_model_id <- best_model_by_metric$solID
 
-print("best_model_id")
-print(best_model_id)
+#print("best_model_id")
+#print(best_model_id)
 
 exit()
 meta_top_models <- filter(meta_top_models, meta_top_models$top_sol == TRUE)$solID
